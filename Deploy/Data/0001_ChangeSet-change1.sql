a2ee99e6e788f869e8570f457f61a33bca800ec6
joshua diaz <joshuad319@gmail.com>
2022-10-12 15:00:42 +0000

    Update 0001_ChangeSet-change1.sql

--- Deploy/Data/0001_ChangeSet-change1.sql
+++ Deploy/Data/0001_ChangeSet-change1.sql
@@ -1,3 +1,3 @@
 --comment: example comment
-CREATE TABLE person (firstname VARCHAR(20), surname VARCHAR(20), address VARCHAR(20), sex CHAR(1), birth DATE);
+CREATE TABLE dbo.person (firstname VARCHAR(20), surname VARCHAR(20), address VARCHAR(20), sex CHAR(1), birth DATE);
 --rollback

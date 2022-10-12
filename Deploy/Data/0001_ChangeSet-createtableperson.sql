bece21976c1bc23c5be0be800eabf7a427fdf3f8
Joshua Diaz <joshuad319@gmail.com>
2022-10-12 14:46:33 -0500

    upload files

---
+++ Deploy/Data/0001_ChangeSet-createtableperson.sql
@@ -0,0 +1,3 @@
+--comment: example comment
+CREATE TABLE dbo.person (firstname VARCHAR(20), surname VARCHAR(20), address VARCHAR(20), gender CHAR(1), birth DATE);
+--rollback
--- Deploy/Data/0002_ChangeSet-insertdataintableperson.sql
+++ Deploy/Data/0002_ChangeSet-insertdataintableperson.sql
@@ -1,4 +1,4 @@
 --comment: example comment
-INSERT INTO dbo.person (firstname, surname, address, sex, birth)
+INSERT INTO dbo.person (firstname, surname, address, gender, birth)
 VALUES ('Juan', 'Perez', 'cra 19 # 120', 'm', '10/12/1998');
 --rollback
